975943e066780a199eb4a24d17c15dd33b1bff2e
Christopher Petrov <developer1@shumen-it.com>
2024-09-08 20:42:16 +0300

    Added bum bum bam bim

--- Na-KomshU-Da-Mu-E-Zle/Assets/Scripts/ChangeScenes.cs
+++ Na-KomshU-Da-Mu-E-Zle/Assets/Scripts/ChangeScenes.cs
@@ -15,7 +15,7 @@ public class ChangeScenes : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        fgdhsfhjgdfgdhnjgfhjk
     }
 
     // Update is called once per frame
